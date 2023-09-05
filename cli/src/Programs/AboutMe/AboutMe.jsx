f52e40a016950301acb51913eb54691677291401
Devon Owen <dowen@tdxtech.com>
2023-09-05 11:38:44 -0500

    added more wording and pseudocode for About me section

--- cli/src/Programs/AboutMe/AboutMe.jsx
+++ cli/src/Programs/AboutMe/AboutMe.jsx
@@ -33,8 +33,71 @@ const SectionThree = () => {
     return (
         <div>
             <h1>My Journey</h1>
+            <p>I embarked on my UX programming journey by studying [Your Relevant Degrees or Courses] at [Your University]. Over the years, I've had the privilege of working on a variety of projects that have honed my skills and deepened my passion for UX programming.</p>
             <div>[Background Image: A timeline or collage showcasing key milestones in my web development career, such as relevant certifications, or significant projects.]</div>
+        </div>
+    )
+}
 
+const SectionFour = () => {
+    return (
+        <div>
+            <h1>My UX Philosophy</h1>
+            <p>I firmly believe that a successful digital product begins with a deep understanding of the user. My approach to UX programming is rooted in meticulous research, thoughtful design, and continuous testing. I strive to create experiences that delight users and exceed their expectations.</p>
+        </div>
+    )
+}
+
+const SectionFive = () => {
+    return (
+        <div>
+            <h1>What drives me</h1>
+            <p>When I'm not coding or designing, you can find me [Hobbies/Interests Outside of Work]. These activities fuel my creativity, inspire new ideas, and provide the balance needed to excel in the fast-paced world of UX programming.</p>
+            <div>[Image: A gallery or collage of images/icons representing your interests and inspirations outside of UX programming, which humanizes your profile.]</div>
+        </div>
+    )
+}
+
+const SectionSix = () => {
+    return (
+        <div className='flex centeredFlex'>
+            <div>[Contact Form or Social Media Icons: Make it easy for visitors to get in touch with you.]</div>
+            <div>
+                <h1>Lets Connect</h1>
+                <p>When I'm not coding or designing, you can find me [Hobbies/Interests Outside of Work]. These activities fuel my creativity, inspire new ideas, and provide the balance needed to excel in the fast-paced world of UX programming.</p>
+            </div>
+        </div>
+    )
+}
+
+const SectionSeven = () => {
+    return(
+        <div>
+            <h1>Testimonials</h1>
+            <div>[Carousel or quotes from clients, colleagues, or team members who can vouch for your UX programming skills and work ethic.]</div>
+            <p>But don't just take my word for it. Here's what some of my collaborators have to say about working with me</p>
+            <div>
+                <p>// "[Client/Colleague Name], [Company]: [Testimonial]"</p>
+                <p>// "[Client/Colleague Name], [Company]: [Testimonial]"</p>
+                <p>// "[Client/Colleague Name], [Company]: [Testimonial]"</p>
+            </div>
+        </div>
+    )
+}
+
+const SectionEight = () => {
+    return (
+        <div>
+            <h1>Thank You</h1>
+            <p>Thank you for exploring my UX programming portfolio and getting to know me better. I'm excited about the opportunity to collaborate with you and contribute to creating exceptional digital experiences.</p>
+        </div>
+    )
+}
+
+const Footer = () => {
+    return (
+        <div>
+            <p>Wording by ChatGPT! AI is the future after all.</p>
         </div>
     )
 }
@@ -50,6 +113,24 @@ export const AboutMe = () => {
             </div>
             <div className='section sectionHeightNormal'>
                 <SectionThree/>
+            </div>
+            <div className='section sectionHeightNormal'>
+                <SectionFour/>
+            </div>
+            <div className='section sectionHeightNormal'>
+                <SectionFive/>
+            </div>
+            <div className='section sectionHeightNormal'>
+                <SectionSix/>
+            </div>
+            <div className='section sectionHeightNormal'>
+                <SectionSeven/>
+            </div>
+            <div className='section sectionHeightNormal'>
+                <SectionEight/>
+            </div>
+            <div className='section sectionHeightNormal'>
+                <Footer/>
             </div>
         </div>
     )
