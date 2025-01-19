6f4907dbf59ac1a4e4e5578f4086eed8f5adfbdf
FallenAngel2021 <dvowen@cox.net>
2025-01-18 21:52:18 -0600

    Adding privacy policy for side hustle

--- cli/src/Programs/App.js
+++ cli/src/Programs/App.js
@@ -1,10 +1,15 @@
 import '../Styles/App.css';
 import { Desktop } from './Desktop/Desktop';
+import { Routes, Route } from 'react-router-dom';
+import PrivacyPolicy from '../Misc/PrivacyPolicy';
 
 function App() {
   return (
     <div className="App">
-      <Desktop/>
+      <Routes>
+        <Route path="/" element={<Desktop/>} />
+        <Route path="/privacyPolicy" element={<PrivacyPolicy />} />
+      </Routes>
     </div>
   );
 }
--- cli/src/index.js
+++ cli/src/index.js
@@ -5,15 +5,17 @@ import App from './Programs/App.js';
 import reportWebVitals from './Scripts/reportWebVitals';
 import { AppsArea } from './Context/appsContext';
 import { ThemeProvider } from './Context/themeContext.js';
-
+import { BrowserRouter as Router } from 'react-router-dom';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <AppsArea>
-      <ThemeProvider>
-        <App />
-      </ThemeProvider>
-    </AppsArea>
+      <Router>
+        <AppsArea>
+          <ThemeProvider>
+            <App />
+          </ThemeProvider>
+        </AppsArea>
+      </Router>
   </React.StrictMode>
 );
 
